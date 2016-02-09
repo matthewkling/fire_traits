01ca76f761a489860c526c511beb72e29d7b2eb1
Matthew Kling <mattkling@gmail.com>
2016-02-08 17:44:52 -0800

    finish first pass at code to read and filter data table

--- TRY_distill.R
+++ TRY_distill.R
@@ -1,26 +1,33 @@
 
 ##### distill raw TRY dataset into usable form covering relevant species
 
+library(data.table)
+library(dplyr)
+library(tidyr)
+
 
 # list of tree species in FFE dataset
-spp <- read.csv("E:/fire_traits/FFE/traits_clean.csv", stringsAsFactors=F)
+spp <- read.csv("E:/fire_traits/derived_data/FFE_traits_clean.csv", stringsAsFactors=F)
 
-# read in TRY data, in chunks due to memory limits, retaining only relevant species
-n <- 1e5
-i <- 0
-status <- "incomplete"
-while(status=="incomplete"){
-      if(i<1){
-            b <- read.delim("E:/fire_traits/TRY/1645_08022016124158/1645.txt", nrows=n)
-            vars <- row.names(b)
-      }else{b <- read.delim("E:/fire_traits/TRY/1645_08022016124158/1645.txt",
-                              skip=i*n+1, nrows=n, row.names=vars)}
-      if(nrow(b)<n) status <- "complete"
-      b <- b[b$SpeciesName %in% spp$scientific_name,]
-      if(i<1) d <- b else(d <- rbind(d, b))
-      i <- i + 1
-}
+# read in TRY data
+tryfile <- "E:/fire_traits/input_data/TRY/1645_08022016124158/1645.txt"
+d <- fread(tryfile)
 
+# filter and summarize
+d <- tbl_df(d) %>%
+      filter(SpeciesName %in% spp$scientific_name,
+             !is.na(TraitID)) %>%
+      select(SpeciesName, TraitName, TraitID, OriglName, OrigValueStr, StdValue) %>%
+      select(SpeciesName, TraitName, StdValue) %>%
+      group_by(SpeciesName, TraitName) %>%
+      summarize(StdValue = mean(na.omit(StdValue))) %>%
+      filter(is.finite(StdValue)) %>%
+      spread(TraitName, StdValue)
 
+# only keep traits with values for at least 25 species
+trait_counts <- apply(d, 2, function(x)length(na.omit(x)))
+d <- d[,names(trait_counts[trait_counts>=25])]
 
+# export csv
+write.csv(d, "E:/fire_traits/derived_data/TRY_traits_clean.csv", row.names=F)
 
